09b671dfe37a7f45878d5533e33aee520468b227
Yusuke Sakurai <kerokerokerop@gmail.com>
2013-11-18 20:10:00 +0900

    Powermate及び左右キーでの移動で移動量を測定出来るようになった

--- PowerMateDial/PMDDebugWindowController.m
+++ PowerMateDial/PMDDebugWindowController.m
@@ -8,7 +8,20 @@
 
 #import "PMDDebugWindowController.h"
 
+// 回転が止まってから何秒後に終了検知するか
+#define FINISH_TIMER_VALUE 0.8
+
+typedef enum NSUInteger{
+    RotationDirectionLeft = 123,
+    RotationDirectionRight = 124
+}RotationDirection;
+
 @interface PMDDebugWindowController ()
+{
+    NSInteger rotationCount;
+    NSTimer *timer;
+}
+
 @property (weak) IBOutlet NSTextField *label;
 
 @end
@@ -29,6 +42,41 @@
     [super windowDidLoad];
     
     // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    [self.window makeFirstResponder:self];
+}
+
+- (void)finishRotation
+{
+    NSLog(@"rotation finished direction:times: %li",(long)rotationCount);
+    rotationCount = 0;
+    [self.label setTextColor:[NSColor redColor]];
+}
+
+-(void)keyDown:(NSEvent *)theEvent
+{
+    NSLog(@"%@",theEvent);
+    // 文字色を戻す
+    if (rotationCount == 0) {
+        [self.label setTextColor:[NSColor whiteColor]];
+    }
+    // 回転終了のチェック
+    [timer invalidate];
+    timer = [NSTimer scheduledTimerWithTimeInterval:FINISH_TIMER_VALUE target:self selector:@selector(finishRotation) userInfo:nil repeats:NO];
+    
+    // 方向によって回転数を上げ下げ
+    switch (theEvent.keyCode) {
+        case RotationDirectionLeft:
+            // left
+            rotationCount--;
+            break;
+        case RotationDirectionRight:
+            // right
+            rotationCount++;
+            break;
+        default:
+            break;
+    }
+    [self.label setIntegerValue:rotationCount];
 }
 
 @end
