b9c5c7639e8ac36504e1568aed5591185a1dfc7f
Sota Sugiura <t11460ss@sfc.keio.ac.jp>
2013-11-18 21:05:18 +0900

    Recipe以外のフォルダにダミーファイルを作成 #5

--- Tree/add_file.py
+++ Tree/add_file.py
@@ -1,8 +1,10 @@
+# encoding:utf-8
+
 import os
 import sys
 
 argvs = sys.argv
 
-# 第一引数:ファイル名 第二引数:拡張子
+# 第一引数:ファイル名 第二引数:拡張子 第三引数:パス
 for i in range(50):
-    f=open(argvs[1]+str(i)+argvs[2],"w")
+    f=open(argvs[3]+argvs[1]+str(i)+argvs[2],"w")
