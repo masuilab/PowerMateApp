71b80809264b6bc74a5aefed93cdc610a1f2b378
Sota Sugiura <t11460ss@sfc.keio.ac.jp>
2013-11-21 02:40:03 +0900

    fix allrecipe.ltsv/make success

--- Data/Recipe/parse_json.py
+++ Data/Recipe/parse_json.py
@@ -6,10 +6,19 @@ import sys
 fp = open('allrecipe.json','rb')
 data = json.load(fp)
 
-ft = open('allrecipe2.ltsv','wb')
+ft = open('allrecipe.ltsv','wb')
 ft.write('title:料理\n') # カレント
 
 for key in data:
     ft.write(' title:'+key.encode('utf-8')+'\n') # カテゴリ
     for key2 in data[key]:
-        ft.write('  title:'+key2['title'].encode('utf-8')+'\turl:'+key2['link'].encode('utf-8')+'\text:html\timage_path:'+key2['iamge_path'].encode('utf-8')+'\n') # タイトル
+        title = key2['title'].encode('utf-8')           # title
+        url = key2['link'].encode('utf-8')              # url
+        image_path = key2['iamge_path'].encode('utf-8') # image_path
+
+        # \s \n \t を洗い出す
+        # \s を - に置換
+        title = title.replace('\n','')
+
+        # ファイルに書き込み
+        ft.write('  title:'+title+'\turl:'+'link'+'\text:html\timage_path:'+image_path+'\n')
