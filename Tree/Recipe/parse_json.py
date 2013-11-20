3cedccece5c47408ddd40b53d640d5be9be91bc6
Sota Sugiura <t11460ss@sfc.keio.ac.jp>
2013-11-20 22:43:51 +0900

    make all folder and files #6

--- Tree/Recipe/parse_json.py
+++ Tree/Recipe/parse_json.py
@@ -3,17 +3,24 @@ import json
 import os
 import sys
 
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
 fp = open('allrecipe.json','rb')
 data = json.load(fp)
-i = 1
 
 for key in data:
-    print key
-    os.mkdir(key)
+    os.makedirs(key.encode('utf-8'))
     for key2 in data[key]:
-        os.makedirs(key+"/"+key2['title'])
-        path = key+"/"+key2['title']+"/recipe"+str(i)
+        print key2['title'].encode('utf-8')
+        path1 = key+"/"+key2['title']
+        print path1
+        try:
+            os.makedirs(path1)
+        except:
+            print "I'm sorry."
+        path2 = key+"/"+key2['title']+"/recipe.txt"
         link = u"レシピのリンク先はこちら -> "+key2['link']+"\n"+u"画像のリンク先はこちら -> "+key2['iamge_path']
-        f=open(path,"w")
+        f=open(path2,"w")
         f.write(link.encode('utf-8'))
         f.close()
