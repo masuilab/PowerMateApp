9e48f31c13528bac0064f1f8cccc28a671efba0a
Sho Hashimoto <hashimoto@shokai.org>
2013-11-20 16:07:57 +0900

    写真を日付で階層化するスクリプトを作った  #17

--- misc/photo_hierarcy/make_hierarcy_photo.rb
+++ misc/photo_hierarcy/make_hierarcy_photo.rb
@@ -1,19 +1,29 @@
+# -*- coding: utf-8 -*-
 require 'rubygems'
 require 'mini_exiftool'
 require 'awesome_print'
+require 'yaml'
 
 def get_create_date(fpath)
   MiniExiftool.new(fpath)["createdate"]
 end
 
 if ARGV.empty?
+  STDERR.puts "please specify photo directory"
   STDERR.puts "e.g.  ruby make_hierarcy_photo.rb /path/to/photodir"
   exit 1
 end
 
 dir = ARGV.shift
-files = Dir.glob("#{dir}/*").select{|i|
-  i =~ /[^sml].jpg$/
-}
+files = Dir.glob(File.join dir, "*").select{|i| i =~ /[^sml].jpg$/ }
 
-puts get_create_date files[0]
+tree = Hash.new{|h,k| h[k] = Hash.new{|h,k| h[k] = Hash.new{|h,k| h[k] = [] } } }
+
+files.each do |file|
+  date = get_create_date file
+  next unless date
+  node = {:url => file, :name => "masui photo"}
+  tree["#{date.year}"]["#{date.year}/#{date.month}"]["#{date.year}/#{date.month}/#{date.day}"].push node
+end
+
+puts tree.to_yaml
